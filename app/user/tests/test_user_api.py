08ce83c8034725ba5e1ce710b13a02cf9d9cccde
Joe <kuchobake@hotmail.com>
2025-01-31 08:00:31 -0800

    added commits for api recipe

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -94,6 +94,14 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email"""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_create_token_blank_password(self):
         """Test posting a blank password returns and error"""
         payload = {'email': 'test@example.com', 'password': ''}
@@ -128,7 +136,7 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             'name': self.user.name,
-            'emal': self.user.email,
+            'email': self.user.email,
         })
 
     def test_post_me_not_allowed(self):
